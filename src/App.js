cb2f6776a5cbd83a772599666934affc6e01f7e9
supachai1998 <61734655+supachai1998@users.noreply.github.com>
2020-12-01 13:54:43 +0700

    fix bug

--- src/App.js
+++ src/App.js
@@ -7,20 +7,16 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./css/app.css";
 import ContentDisplay from "./component/ContentDisplay";
 import Management from "./component/Management";
-import Loading from './component/Loading'
+import Loading from "./component/Loading";
 const App = () => {
   const [content, setcontent] = useState([]);
   const [show, setshow] = useState(false);
   const [displayAll, setdisplayAll] = useState(false);
+  
 
-  React.useEffect(() => {
+  React.useEffect(async () => {
     !firebase.apps.length ? firebase.initializeApp(config) : firebase.app();
-    query()
-  },[]);
-
-  const query = async () => {
-    setcontent(null)
-     const userRef = firebase.database().ref("history").orderByChild('status');
+    const userRef = firebase.database().ref("history").orderByChild("status");
     let newQuery = [];
     await userRef.on("value", (snapshot) => {
       snapshot.forEach((data) => {
@@ -29,7 +25,7 @@ const App = () => {
         let name = "";
         userRef2.on("value", (snapshot2) => {
           snapshot2.forEach((data2) => {
-            name= data2.val();
+            name = data2.val();
           });
         });
         // console.log(newQuery2)
@@ -40,37 +36,82 @@ const App = () => {
           date: dataVal.date,
           detail: dataVal.detail,
           status: dataVal.status,
+          his_qry: new Date(),
+        });
+      });
+      // console.log(newQuery)
+      setcontent(newQuery);
+      console.log(content)
+    });
+    
+  }, []);
+
+  const query = async () => {
+    
+    const userRef = firebase.database().ref("history").orderByChild("status");
+    let newQuery = [];
+    await userRef.on("value", (snapshot) => {
+      snapshot.forEach((data) => {
+        setcontent([])
+        const dataVal = data.val();
+        const userRef2 = firebase.database().ref(`plants`).child(dataVal.code);
+        let name = "";
+        userRef2.on("value", (snapshot2) => {
+          snapshot2.forEach((data2) => {
+            name = data2.val();
+          });
+        });
+        // console.log(newQuery2)
+        newQuery.push({
+          id: data.key,
+          code: dataVal.code,
+          name: name,
+          date: dataVal.date,
+          detail: dataVal.detail,
+          status: dataVal.status,
+          his_qry: new Date(),
         });
       });
       // console.log(newQuery)
       setcontent(newQuery);
     });
-
+    
     // console.log(content)
   };
 
   return (
     <Jumbotron>
       {/* {AppCarousel(content)} */}
-      <Management show={show} setshow={setshow} firebase={firebase} query={query}/>
+      <Management
+        show={show}
+        setshow={setshow}
+        firebase={firebase}
+        query={query}
+      />
 
-        <h2 className="float-left">แปลงผัก</h2>
-        
-        <Button
-          variant="outline-success"
-          className="float-right"
-          onClick={() => {setshow(true); query()}}
-        >
-          Add
-        </Button>
-        <Button
-          variant="outline-info"
-          className="float-right mr-2"
-          onClick={() => {query(); setdisplayAll(prev => !prev);}}
-        >
-          {!displayAll ? "show All" : " Close"}
-        </Button>
-        <hr className="bg-dark mt-5" />
+      <h2 className="float-left">แปลงผัก</h2>
+
+      <Button
+        variant="outline-success"
+        className="float-right"
+        onClick={() => {
+          setshow(true);
+          // query();
+        }}
+      >
+        Add
+      </Button>
+      <Button
+        variant="outline-info"
+        className="float-right mr-2"
+        onClick={() => {
+          // query();
+          setdisplayAll((prev) => !prev);
+        }}
+      >
+        {!displayAll ? "show All" : " Close"}
+      </Button>
+      <hr className="bg-dark mt-5" />
       {!content || content.length === 0 ? (
         <Loading />
       ) : (
@@ -85,20 +126,20 @@ const App = () => {
             </tr>
           </thead>
           <tbody>
-            {content && content.map(({code, name, date, detail, id,status}) => (
-            <ContentDisplay
-              key={id}
-              id={id}
-              date={date}
-              code={code}
-              name={name}
-              detail={detail}
-              firebase={firebase}
-              query={query}
-              status={status}
-              displayAll={displayAll}
-            />
-            ))}
+            {content &&
+              content.map((index,item) => (
+                <>
+                <ContentDisplay
+                  key={index.id}
+                  content={index}
+                  firebase={firebase}
+                  query={query}
+                  displayAll={displayAll}
+                  setcontent={setcontent}
+
+                />
+                </>
+              ))}
           </tbody>
         </Table>
       )}
@@ -135,5 +176,3 @@ const AppCarousel = (content) => {
     </div>
   );
 };
-
-
